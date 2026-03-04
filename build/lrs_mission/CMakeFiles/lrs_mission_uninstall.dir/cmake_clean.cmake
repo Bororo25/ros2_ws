f9a98bd91bb2b3b3140fdb53cc4150208394d6ea
Martin Brandobur <martinbrandobur@gmail.com>
2026-03-04 17:40:03 +0100

    Uprava

--- build/lrs_mission/CMakeFiles/lrs_mission_uninstall.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/lrs_mission_uninstall"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/lrs_mission_uninstall.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- build/lrs_mission/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/lrs/ros2_ws
-# Build directory: /home/lrs/ros2_ws/build/lrs_mission
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
